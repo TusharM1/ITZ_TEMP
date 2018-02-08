b9a55db4d342220a5758a3692573370a4387be13
TusharM1 <tman08810@gmail.com>
2018-02-08 16:04:52 -0500

    Add files via upload

---
+++ include/opControl.h
@@ -0,0 +1,15 @@
+#ifndef OPCONTROL_H_
+#define OPCONTROL_H_
+
+#define MOTOR_BASE_LF 2
+#define MOTOR_BASE_LB 4
+#define MOTOR_BASE_RF 5
+#define MOTOR_BASE_RB 7
+#define LIFT_L 8
+#define LIFT_R 9
+#define CLAW 10
+
+void getMotors();
+void setMotors();
+
+#endif
